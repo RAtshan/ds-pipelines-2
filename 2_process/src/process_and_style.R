f21130d9d76247c65ade230c3b4989f498dce372
Atshan <ratshan@usgs.gov>
2020-06-23 17:42:26 -0500

    Line 1: Changed the argument name for `process_data` function to `site_data_clean_outpath`. To distinguish it’s the output target.
    Line 5 & 6: Same as above.
    
    Line 19:22 -->
    a) The `styled_data` fixed the function,
    b) Changed argument name to `style_data_outpath`.
    c) Used `data.frame` to ensure the product is a data frame.
    
    Line 9:11 --> Changed the `annotate_data` argument name to `site_data_clean_inpath`.

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,14 +1,14 @@
-process_data <- function(filepath, nwis_data){
+process_data <- function(site_data_clean_outpath, nwis_data){
   nwis_data_clean <- rename(nwis_data, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, tz_cd)
   
-  write_csv(nwis_data_clean, path = filepath)
-  return(filepath)
+  write_csv(nwis_data_clean, path = site_data_clean_outpath)
+  return(site_data_clean_outpath)
 }
 
-annotate_data <- function(site_data_clean_path, site_filename){
+annotate_data <- function(site_data_clean_inpath, site_filename){
   site_info <- read_csv(site_filename)
-  site_data_clean <- read_csv(site_data_clean_path)
+  site_data_clean <- read_csv(site_data_clean_inpath)
   annotated_data <- left_join(site_data_clean, site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va)
   
@@ -16,8 +16,7 @@ annotate_data <- function(site_data_clean_path, site_filename){
 }
 
 
-style_data <- function(style_data_filepath, site_data_annotated){
-  site_data_annotated_out <- read_csv(style_data_filepath)
-  mutate(site_data_annotated_out, station_name = as.factor(station_name))
-  return(style_data_filepath)
+style_data <- function(style_data_outpath, site_data_annotated){
+ site_data_annotated_styled <- mutate(site_data_annotated, station_name = as.factor(station_name))
+ data.frame(site_data_annotated_styled)
 }
